7b80e3c93910ccd2c6d8ef971c3ce1cc2f4e8ad8
kelvin926 <aa645900@naver.com>
2021-10-25 05:35:13 +0900

    1장 추가

---
+++ for_1st_exam/ex2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Hello\n";
+	std::cout << "첫 번째 맛보기입니다\n";
+
+	int n = 3;
+	char c = '#';
+	std::cout << c << 5.5 << '-' << n << "hello" << true;
+	return 0;
+}
