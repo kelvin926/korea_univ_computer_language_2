3f23bf5ebb614e8b9502141505072112004f9bb5
kelvin926 <kelvin926@naver.com>
2021-11-08 20:23:45 +0900

    3차 과제

---
+++ 과제03/ch05_01.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+bool bigger(int a, int b, int& big) {
+    if(a==b)
+        return true;
+    else {
+        if(a>b) 
+            big = a;
+        else 
+            big = b;
+        return false;
+    }
+
+}
+int main() {
+    int a, b, big;
+    cout << "2개의 정수를 입력 >> ";
+    cin >> a >> b;
+    if(!bigger(a, b, big)) 
+        cout << "큰 수는 : " << big;
+    else 
+        cout << "두 정수가 같습니다."; 
+}
